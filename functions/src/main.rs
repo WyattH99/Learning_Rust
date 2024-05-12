619ba460ba3df67fd403e1f244939fbff2c6bc31
Wyatt-Desktop <WyattHansen99@gmail.com>
2024-05-12 15:07:25 -0500

    Completed first half of Control Flow

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -10,6 +10,30 @@ fn main() {
 
     let x = plus_one(5);
     println!("plus_one: {x}");
+
+    
+    // Control Flow
+    let number = 8;
+    println!("number: {number}");
+    if number < 5 {
+        println!("number < 5: true");
+    } else {
+        println!("number < 5: false");
+    }
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is : {number}");
 }
 
 fn another_function(x: i32) {
