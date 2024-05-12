884d76a0fd5fe6febd805fad4914052f70f184ba
Wyatt-Desktop <WyattHansen99@gmail.com>
2024-05-12 12:42:15 -0500

    Completed Functions, and Comments!

---
+++ functions/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function(5);
+
+    print_labeled_measurement(5, 'h');
+    
+    let five = five();
+    println!("five(): {five}");
+
+    let x = plus_one(5);
+    println!("plus_one: {x}");
+}
+
+fn another_function(x: i32) {
+    println!("Another function, x: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
