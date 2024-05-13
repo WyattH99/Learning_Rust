082292da16d33166cf651ebc2d22fcb1760c5f77
Wyatt-Desktop <WyattHansen99@gmail.com>
2024-05-12 20:22:54 -0500

    Completed Fahrenheit to Celsius program: f_to_c

---
+++ f_to_c/target/debug/deps/f_to_c-e737ec97bc0cc729.d
@@ -0,0 +1,5 @@
+/home/wyatt/Desktop/Learning_Rust/f_to_c/target/debug/deps/f_to_c-e737ec97bc0cc729: src/main.rs
+
+/home/wyatt/Desktop/Learning_Rust/f_to_c/target/debug/deps/f_to_c-e737ec97bc0cc729.d: src/main.rs
+
+src/main.rs:
